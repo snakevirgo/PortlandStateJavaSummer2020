bbf8e0134623f162a50518bf7244c61d6ec8b518
irisn <iris.ngarden@gmail.com>
2020-07-06 13:58:11 -0700

    added phone bill class

---
+++ phonebill/src/main/java/edu/pdx/cs410J/yal/PhoneBill.java
@@ -0,0 +1,22 @@
+package edu.pdx.cs410J.yal;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+
+import java.util.Collection;
+
+public class PhoneBill extends AbstractPhoneBill<PhoneCall> {
+    @Override
+    public String getCustomer() {
+        return null;
+    }
+
+    @Override
+    public void addPhoneCall(PhoneCall phoneCall) {
+
+    }
+
+    @Override
+    public Collection<PhoneCall> getPhoneCalls() {
+        return null;
+    }
+}
