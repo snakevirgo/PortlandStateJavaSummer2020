8b25fdc300b7c12af94084810186c48dc86655a5
irisn <iris.ngarden@gmail.com>
2020-07-07 23:55:32 -0700

    added a for loop for args read in and print

--- phonebill/src/main/java/edu/pdx/cs410J/yal/Project1.java
+++ phonebill/src/main/java/edu/pdx/cs410J/yal/Project1.java
@@ -6,12 +6,38 @@ package edu.pdx.cs410J.yal;
 public class Project1 {
 
   public static void main(String[] args) {
-    PhoneCall call = new PhoneCall(null);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
+    //PhoneCall call = new PhoneCall(null);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+
+    /*String customer1;
+    String caller1;
+    String callee1;
+    String start1;
+    String end1;
+    String variable = args[8];
+
+    if(args[0] == "-print")
+    {
+
+    }
+     */
+
+ //System.out.println(call);
+
+    if( args.length == 0) {
+      System.err.println("Missing command line arguments");
+    }
+
+    for(int i = 0; i < args.length; ++i)
+    {
+      System.out.println(args[i]);
+    }
+    /*
     for (String arg : args) {
       System.out.println(arg);
-    }
+    }*/
+
     System.exit(1);
+
   }
 
 }
