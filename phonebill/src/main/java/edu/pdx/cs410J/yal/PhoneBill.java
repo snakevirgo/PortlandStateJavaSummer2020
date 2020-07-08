668a8ce20125019d643d9b738656ac1cb6fb3865
irisn <iris.ngarden@gmail.com>
2020-07-07 23:53:04 -0700

    implemented the phone bill functions

--- phonebill/src/main/java/edu/pdx/cs410J/yal/PhoneBill.java
+++ phonebill/src/main/java/edu/pdx/cs410J/yal/PhoneBill.java
@@ -4,19 +4,31 @@ import edu.pdx.cs410J.AbstractPhoneBill;
 
 import java.util.Collection;
 
+import java.util.ArrayList;
+
 public class PhoneBill extends AbstractPhoneBill<PhoneCall> {
+
+    private final String customer;
+    private ArrayList<PhoneCall> calls;
+
+    //customer constructor
+    public PhoneBill(String customer, ArrayList<PhoneCall> calls) {
+        this.customer = customer;
+        this.calls = calls;
+    }
+
     @Override
     public String getCustomer() {
-        return null;
+        return customer;
     }
 
     @Override
     public void addPhoneCall(PhoneCall phoneCall) {
-
+        calls.add(phoneCall);
     }
 
     @Override
     public Collection<PhoneCall> getPhoneCalls() {
-        return null;
+        return calls;
     }
 }
